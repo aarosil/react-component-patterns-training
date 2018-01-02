3dc2488a898773b565abf35f6d6c017909b1587d
Aaron Siladi <aarons@taskrabbit.com>
2018-01-02 13:20:35 -0800

    add focus method to MyToggle to focus the button

--- src/components/MyToggle.js
+++ src/components/MyToggle.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { withToggle } from './Toggle/Toggle';
 
-const MyToggle = ({on, toggle}) =>
-  <div>
-    <button onClick={toggle}>
-      {on ? 'on' : 'off'}
-    </button>
-  </div>
+class MyToggle extends Component {
+  focus = () => this.button.focus()
+
+  render() {
+    const { on, toggle } = this.props;
+
+    return (
+      <div>
+        <button ref={el => this.button = el} onClick={toggle}>
+          {on ? 'on' : 'off'}
+        </button>
+      </div>
+    )
+
+  }
+}
 
 MyToggle.Subtext = () =>
   <span>nooooo</span>
